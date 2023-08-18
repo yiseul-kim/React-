2f37e671df4a487bdde71cb159dcf2769e361266
yiseul_notebook <kimdew402@gmail.com>
2023-08-18 18:16:02 +0900

    array,object state 변경하는 법 까지

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
+/* eslint-disable */
+
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let day = '8월 18일 발행';
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [좋아요, 좋아요변경] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="black-nav">
+        <h4> ReactBLOG </h4>
+      </div>
+
+      <div className="list">
+        <button onClick={()=>{
+          let copy = [...글제목];
+          copy[0]='여자코트 추천';
+          글제목변경(copy);
+        }}>😎글수정</button>
+        <h4 >{ 글제목[0] } <br/>
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <span onClick={ ()=>{
+          좋아요변경(좋아요+1)
+        }
+         }>💕</span> {좋아요} </p></h4>
+         
+        <p>{day}</p>
+      </div>
+
+      <div className="list">
+        <h4>{ 글제목[1] }</h4>
+        <p>{day}</p>
+      </div>
+
+      <div className="list">
+        <h4>{ 글제목[2] }</h4>
+        <p>{day}</p>
+      </div>
+      
     </div>
   );
 }
